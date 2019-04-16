574aaae583bdc71f7d91757e47ea1b8440d94462
renmengqisheng <280947007@qq.com>
2019-04-16 20:30:36 +0800

    new version

--- main.cpp
+++ main.cpp
@@ -25,6 +25,8 @@ FisheyeParam ocam_model1, ocam_model2;
 float theta1 = 45*CV_PI/180, theta2 = -45*CV_PI/180;
 cv::Matx33d R1, R2, R, R_l, R_r;
 cv::Vec3d t1, t2, t;
+cv::Vec3f euler1, euler2;
+cv::Mat Rleft, Rright, Rfront, Rback;
 
 void readTransformation(std::string filename, cv::Matx33d& R, cv::Vec3d& t)
 {
@@ -277,6 +279,162 @@ void twoFisheye2twoPerspective(cv::Mat src1, cv::Mat src2, cv::Mat dst1, cv::Mat
     }
 }
 
+// Calculates rotation matrix given euler angles.
+cv::Mat eulerAnglesToRotationMatrix(cv::Vec3f theta)
+{
+    // Calculate rotation about x axis
+    cv::Mat R_x = (cv::Mat_<double>(3,3) <<
+               1,       0,              0,
+               0,       cos(theta[0]),   -sin(theta[0]),
+               0,       sin(theta[0]),   cos(theta[0])
+               );
+ 
+    // Calculate rotation about y axis
+    cv::Mat R_y = (cv::Mat_<double>(3,3) <<
+               cos(theta[1]),    0,      sin(theta[1]),
+               0,               1,      0,
+               -sin(theta[1]),   0,      cos(theta[1])
+               );
+ 
+    // Calculate rotation about z axis
+    cv::Mat R_z = (cv::Mat_<double>(3,3) <<
+               cos(theta[2]),    -sin(theta[2]),      0,
+               sin(theta[2]),    cos(theta[2]),       0,
+               0,               0,                  1);
+ 
+ 
+    // Combined rotation matrix
+    cv::Mat R = R_z * R_y * R_x;
+ 
+    return R;
+}
+
+// Checks if a matrix is a valid rotation matrix.
+bool isRotationMatrix(cv::Mat &R)
+{
+    cv::Mat Rt;
+    transpose(R, Rt);
+    cv::Mat shouldBeIdentity = Rt * R;
+    cv::Mat I = cv::Mat::eye(3,3, shouldBeIdentity.type());
+ 
+    return  norm(I, shouldBeIdentity) < 1e-5;
+}
+
+// Calculates rotation matrix to euler angles(the order is z first, then x, y last).
+cv::Vec3f rotationMatrixToEulerAngles(cv::Mat &R)
+{
+    assert(isRotationMatrix(R));
+ /*
+    float x, y, z;
+    x = asin(-R.at<double>(1,2));
+    y = atan2(R.at<double>(0,2), R.at<double>(2,2));
+    z = atan2(R.at<double>(1,0), R.at<double>(1,1));
+*/
+    float sy = sqrt(R.at<double>(0,0) * R.at<double>(0,0) +  R.at<double>(1,0) * R.at<double>(1,0));
+    bool singular = sy < 1e-6; // If
+ 
+    float x, y, z;
+    if (!singular)
+    {
+        x = atan2(R.at<double>(2,1) , R.at<double>(2,2));
+        y = atan2(-R.at<double>(2,0), sy);
+        z = atan2(R.at<double>(1,0), R.at<double>(0,0));
+    }
+    else
+    {
+        x = atan2(-R.at<double>(1,2), R.at<double>(1,1));
+        y = atan2(-R.at<double>(2,0), sy);
+        z = 0;
+    }
+
+    return cv::Vec3f(x, y, z);
+}
+
+void upraiseCamera(cv::Mat src, cv::Mat dst, cv::Vec3f euler, float fx, float fy, float xc, float yc, FisheyeParam ocam)
+{
+  int width = src.cols, height = src.rows;
+
+  cv::Matx33d Rz(cos(euler(2)), -sin(euler(2)), 0, sin(euler(2)), cos(euler(2)), 0, 0, 0, 1);
+  cv::Matx33d Ry(cos(euler(1)), 0, sin(euler(1)), 0, 1, 0, -sin(euler(1)), 0, cos(euler(1))); //= (cv::Mat_<double>(3,3) << cos(euler(2)), 0, sin(euler(2)), 0, 1, 0, -sin(euler(2)), 0, cos(euler(2)));
+  cv::Matx33d Rx(1, 0, 0, 0, cos(euler(0)), -sin(euler(0)), 0, sin(euler(0)), cos(euler(0)));
+
+  float theta = CV_PI/2;
+  cv::Matx33d Rrect(1, 0, 0, 0, cos(theta), -sin(theta), 0, sin(theta), cos(theta));
+  cv::Matx33d Ryleft(cos(theta1), 0, sin(theta1), 0, 1, 0, -sin(theta1), 0, cos(theta1));
+  cv::Matx33d Ryright(cos(theta2), 0, sin(theta2), 0, 1, 0, -sin(theta2), 0, cos(theta2));
+
+  float fx1 = fx, fx2 = fx, fy1 = fy, fy2 = fy;
+  cv::Vec3f center(0, 0, 1);
+  cv::Matx33d Rymean = Ryleft * Ryright.t();
+  cv::Vec3d rvec = cv::Affine3d(Rymean).rvec() * 0.5;
+  cv::Rodrigues(rvec, Rymean);
+  cv::Vec3f new_center1 = Rymean.t() * Rrect * Rx.t() * Ry.t() * center;
+  cv::Vec3f new_center2 = Rymean * Rrect * Rx.t() * Ry.t() * center;
+  float xc1 = xc + fx1 * new_center1(0)/new_center1(2), yc1 = yc + fy1 * new_center1(1)/new_center1(2);
+  float xc2 = xc + fx2 * new_center2(0)/new_center2(2), yc2 = yc + fy2 * new_center2(1)/new_center2(2);
+
+  for(int i = 0; i < height; i++)
+    for(int j =0; j < width; j++)
+    {
+      float x, y, z;
+      cv::Vec3f world_point;//(x,y,z);
+      if(j <= xc)
+      {
+        world_point(0) = (j-xc1)/fx1;
+        world_point(1) = (i-yc1)/fy1;
+        world_point(2) = 1;
+        world_point = Ry.t() * world_point;
+        world_point = Rx.t() * world_point;
+        world_point = Rrect * world_point;
+        world_point = Ryleft.t() * world_point;
+      }
+      else
+      {
+        world_point(0) = (j-xc2)/fx2;
+        world_point(1) = (i-yc2)/fy2;
+        world_point(2) = 1;
+        world_point = Ry.t() * world_point;
+        world_point = Rx.t() * world_point;
+        world_point = Rrect * world_point;
+        world_point = Ryright.t() * world_point;
+      }
+      
+      //world_point = Rz.t() * world_point;
+      //world_point = Ry.t() * world_point;
+      //world_point = Rx.t() * world_point;
+      //world_point = Rrect * world_point;
+      cv::Point2f pix = ocam.World2Camera(cv::Point3f(world_point));
+      int u = pix.x, v = pix.y;
+      if(u >=0 && u < width && v >= 0 && v < height)
+        dst.at<cv::Vec3b>(i,j) = src.at<cv::Vec3b>(v,u);
+    }
+}
+
+void correctCameraWithRotation(cv::Mat src, cv::Mat dst, cv::Mat R, float fx, float fy, float xc, float yc, FisheyeParam ocam)
+{
+  int width = src.cols, height = src.rows;
+  // cv::Vec3f p(xc, yc, 1);
+  // cv::Matx33d R_;
+  // R.convertTo(R_, CV_64F);
+  // p = R_ * p;
+  // xc = p(0);
+  // yc = p(1);
+  for(int i = 0; i < height; i++)
+    for(int j =0; j < width; j++)
+    {
+      float x, y, z;
+      x = (j-xc)/fx;
+      y = (i-yc)/fy;
+      z = 1;
+      cv::Mat world_point = (cv::Mat_<double>(3,1) << x,y,z);
+      world_point = R * world_point;
+      cv::Point2f pix = ocam.World2Camera(cv::Point3f(world_point));
+      int u = pix.x, v = pix.y;
+      if(u >=0 && u < width && v >= 0 && v < height)
+        dst.at<cv::Vec3b>(i,j) = src.at<cv::Vec3b>(v,u);
+    }
+}
+
 void Onchange(int, void*)
 {
   K1 = (cv::Mat_<float>(3, 3) << fx,  0, xc,
@@ -284,9 +442,14 @@ void Onchange(int, void*)
                                  0,  0, 1);
   dst1 = cv::Mat(src1.rows, src1.cols, src1.type(), cv::Scalar::all(0));
   dst2 = cv::Mat(src2.rows, src2.cols, src2.type(), cv::Scalar::all(0));
-  Fisheye2Perspective(src1, dst1, fx, fy, R_r, ocam_model1);
-  Fisheye2Perspective(src2, dst2, fx, fy, R_l, ocam_model2);
+  //Fisheye2Perspective(src1, dst1, fx, fy, R_r, ocam_model1);
+  //Fisheye2Perspective(src2, dst2, fx, fy, R_l, ocam_model2);
   //twoFisheye2twoPerspective(src1, src2, dst1, dst2, fx, fy, R_r, R_l, ocam_model1, ocam_model2);
+  upraiseCamera(src1, dst1, euler1, fx, fy, xc, yc, ocam_model1);
+  upraiseCamera(src2, dst2, euler2, fx, fy, xc, yc, ocam_model2);
+  //correctCameraWithRotation(src1, dst1, Rleft, fx, fy, xc, yc, ocam_model1);
+  //correctCameraWithRotation(src2, dst2, Rfront, fx, fy, xc, yc, ocam_model2);
+
   cv::imshow( "Undistorted Perspective Image1", dst1 );
   cv::imshow( "Undistorted Perspective Image2", dst2 );
 }
@@ -296,20 +459,20 @@ int main(int argc, char *argv[])
   /* --------------------------------------------------------------------*/
   /* Read the parameters of the omnidirectional camera from the TXT file */
   /* --------------------------------------------------------------------*/
-  ocam_model1.Load("../intrinsic_parameters/left/calib.txt");
-  ocam_model2.Load("../intrinsic_parameters/front/calib.txt");
+  ocam_model1.Load("../intrinsic_parameters/right/calib.txt");
+  ocam_model2.Load("../intrinsic_parameters/back/calib.txt");
 
   /* --------------------------------------------------------------------*/
   /* Allocate space for the unistorted images                            */
   /* --------------------------------------------------------------------*/
-  src1 = cv::imread("../bmp/frame_vc9_1814.bmp");      // source image 1
+  src1 = cv::imread("../bmp/frame_vc11_1814.bmp");      // source image 1
   assert(!src1.empty());
-  src2 = cv::imread("../bmp/frame_vc10_1814.bmp");      // source image 1
+  src2 = cv::imread("../bmp/frame_vc12_1814.bmp");      // source image 1
   assert(!src2.empty());
 
   //cv::Matx44d T1, T2;
-  readTransformation("../result/left.txt", R1, t1);
-  readTransformation("../result/front.txt", R2, t2);
+  readTransformation("../result/right.txt", R1, t1);
+  readTransformation("../result/back.txt", R2, t2);
   //mapx1 = cv::Mat(src1.rows, src1.cols, CV_32FC1);
   //mapy1 = cv::Mat(src1.rows, src1.cols, CV_32FC1);
 
@@ -331,9 +494,20 @@ int main(int argc, char *argv[])
   R = R2 * R1.t();
   t = t2 - R * t1;
   calculateTwoRotation(R, t, R_l, R_r);
-  Fisheye2Perspective(src1, dst1, fx, fy, R_r, ocam_model1);
-  Fisheye2Perspective(src2, dst2, fx, fy, R_l, ocam_model2);
+  //Fisheye2Perspective(src1, dst1, fx, fy, R_r, ocam_model1);
+  //Fisheye2Perspective(src2, dst2, fx, fy, R_l, ocam_model2);
   //twoFisheye2twoPerspective(src1, src2, dst1, dst2, fx, fy, R_r, R_l, ocam_model1, ocam_model2);
+  cv::Mat R1_mat, R2_mat;
+  cv::Mat(R1, false).convertTo(R1_mat, CV_64F);
+  cv::Mat(R2, false).convertTo(R2_mat, CV_64F);
+  //Rleft = eulerAnglesToRotationMatrix(cv::Vec3f(CV_PI/4, 0, 0));// * R1_mat.t();
+  //Rfront = eulerAnglesToRotationMatrix(cv::Vec3f(CV_PI/4, 0, 0));// * R2_mat.t();
+  //correctCameraWithRotation(src1, dst1, Rleft, fx, fy, xc, yc, ocam_model1);
+  //correctCameraWithRotation(src2, dst2, Rfront, fx, fy, xc, yc, ocam_model2);
+  euler1 = rotationMatrixToEulerAngles(R1_mat);
+  euler2 = rotationMatrixToEulerAngles(R2_mat);
+  upraiseCamera(src1, dst1, euler1, fx, fy, xc, yc, ocam_model1);
+  upraiseCamera(src2, dst2, euler2, fx, fy, xc, yc, ocam_model2);
 
   
   cv::namedWindow( "Original fisheye camera image1", 0 );
@@ -343,8 +517,8 @@ int main(int argc, char *argv[])
   cv::namedWindow( "toolbox", 0 );
 
   cv::imshow( "Original fisheye camera image1", src1 );
-  cv::imshow( "Undistorted Perspective Image1", dst1 );
   cv::imshow( "Original fisheye camera image2", src2 );
+  cv::imshow( "Undistorted Perspective Image1", dst1 );
   cv::imshow( "Undistorted Perspective Image2", dst2 );
 
   cv::createTrackbar("fx", "toolbox", &fx, slider_max, Onchange);
